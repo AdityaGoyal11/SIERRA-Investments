fcb916017a51ea0ee535b7d80b0ef6a3a769114a
k3vin333 <k3vinalt@hotmail.com>
2025-03-02 16:50:23 +1100

    initialized basic express setup and updated readme for easy understanding of setup

--- app/lambda/index.js
+++ app/lambda/index.js
@@ -1,7 +1,11 @@
 exports.handler = async (event) => {
-    // TODO: Implement logic to process ESG data from S3 bucket and save it to DynamoDB
-    return {
-        statusCode: 200,
-        body: JSON.stringify("ESG Lambda function to extract data from S3 csv data to DynamoDB executed successfully"),
+  // TODO: Implement ESG data processing logic
+  // 1. Get CSV from S3
+  // 2. Process the data
+  // 3. Store in DynamoDB
+  
+  return {
+    statusCode: 200,
+    body: JSON.stringify("ESG Lambda function to extract data from S3 csv data to DynamoDB executed successfully"),
     };
 };
